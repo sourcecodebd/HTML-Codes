d6a24b992da5a8b4114d1df1f40b5365f31d7a69
sourcecodebd <nafimahmud1996@gmail.com>
2021-07-20 01:35:27 +0600

    1st commit

---
+++ Area.php
@@ -0,0 +1,16 @@
+
+<!doctype html>
+<html>
+    <body>
+    <h1>Area-Perimeter</h1>
+    <?php
+    
+    $l=4;
+    $b=2;
+    $a=$l*$b;
+    $p=2*($l+$b);
+       echo "Area: ". $a . "<br>";
+       echo "Perimeter: ". $p . "<br>";
+    ?>
+    </body>
+</html>
---
+++ Odd.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <body>
+    <?php
+for($o=11;$o<100;)
+{
+echo "Odd Number from 10-100 : ". $o . "<br>";
+$o=$o+2;
+}
+    ?>
+    </body>
+</html>
